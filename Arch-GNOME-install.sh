82cd58a330b70e8e9c0b1e043800ae7a0f9a7146
Jesse Steele <9209377+JesseSteele@users.noreply.github.com>
2026-03-10 23:06:04 +0000

    Dark theme for legacy apps

--- Arch-GNOME-install.sh
+++ Arch-GNOME-install.sh
@@ -4,6 +4,6 @@
 
 /usr/bin/sed -i "s/^#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/" /etc/locale.gen
 /usr/bin/locale-gen
-/usr/bin/pacman -S --needed --noconfirm gnome-console gnome-terminal gdm libva-utils gnome gnome-shell gnome-control-center gnome-extra gnome-tweaks extension-manager gnome-browser-connector gnome-shell-extensions gnome-shell-extension-dash-to-panel gnome-shell-extension-appindicator gnome-text-editor gedit htop ntp guake chromium
+/usr/bin/pacman -S --needed --noconfirm gnome-console gnome-terminal gdm libva-utils gnome gnome-shell gnome-control-center gnome-extra gnome-tweaks extension-manager gnome-browser-connector gnome-shell-extensions gnome-shell-extension-dash-to-panel gnome-shell-extension-appindicator gnome-themes-extra gnome-text-editor gedit htop ntp guake chromium
 /usr/bin/pacman -S --noconfirm noto-fonts noto-fonts-cjk noto-fonts-emoji noto-fonts-extra ttf-jetbrains-mono ttf-fira-code ttf-hack adobe-source-code-pro-fonts inter-font ttf-ubuntu-font-family
 /usr/bin/systemctl enable gdm
