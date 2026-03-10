4867e49f657619a7aa73db2d7054a59cb83a515c
Jesse Steele <9209377+JesseSteele@users.noreply.github.com>
2026-03-10 22:09:08 +0000

    Remove AUR installs

--- Arch-GNOME-install.sh
+++ Arch-GNOME-install.sh
@@ -6,12 +6,4 @@
 /usr/bin/locale-gen
 /usr/bin/pacman -S --needed --noconfirm gnome-console gnome-terminal gdm libva-utils gnome gnome-shell gnome-control-center gnome-extra gnome-tweaks extension-manager gnome-browser-connector gnome-shell-extensions gnome-shell-extension-dash-to-panel gnome-shell-extension-appindicator gnome-text-editor gedit htop ntp guake chromium
 /usr/bin/pacman -S --noconfirm noto-fonts noto-fonts-cjk noto-fonts-emoji noto-fonts-extra ttf-jetbrains-mono ttf-fira-code ttf-hack adobe-source-code-pro-fonts inter-font ttf-ubuntu-font-family
-/usr/bin/git clone https://github.com/jessesteele/yeo
-cd yeo
-/usr/bin/chmod +x install.sh
-./install.sh
-cd ..
-/usr/bin/rm -rf yeo
-/usr/bin/sudo -u worker /bin/bash -c "/usr/bin/yay -S --noconfirm ttf-ms-fonts ttf-mac-fonts nerd-fonts-complete"
-/usr/bin/sudo -u worker /bin/bash -c '/usr/bin/yay -S --noconfirm xfce4-terminal gnome-shell-extension-system-monitor-applet'
 /usr/bin/systemctl enable gdm
