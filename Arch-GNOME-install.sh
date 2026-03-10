1968e87cfdef77c09a2bb8b593242ec6b735cc87
Jesse Steele <9209377+JesseSteele@users.noreply.github.com>
2026-03-10 21:56:06 +0000

    Type-o

--- Arch-GNOME-install.sh
+++ Arch-GNOME-install.sh
@@ -4,7 +4,7 @@
 
 /usr/bin/sed -i "s/^#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/" /etc/locale.gen
 /usr/bin/locale-gen
-/usr/bin/pacman -S --needed --noconfirm gnome-console gnome-terminal gdm libva-utils gnome gnome-shell gnome-control-center gnome-extra gnome-tweaks extension-manager gnome-browser-connector gnome-shell-extensions gnome-shell-extension-dash-to-panel gnome-shell-extension-appindicator gnome-text-editor gedit top ntp guake chromium
+/usr/bin/pacman -S --needed --noconfirm gnome-console gnome-terminal gdm libva-utils gnome gnome-shell gnome-control-center gnome-extra gnome-tweaks extension-manager gnome-browser-connector gnome-shell-extensions gnome-shell-extension-dash-to-panel gnome-shell-extension-appindicator gnome-text-editor gedit htop ntp guake chromium
 /usr/bin/pacman -S --noconfirm noto-fonts noto-fonts-cjk noto-fonts-emoji noto-fonts-extra ttf-jetbrains-mono ttf-fira-code ttf-hack adobe-source-code-pro-fonts inter-font ttf-ubuntu-font-family
 /usr/bin/git clone https://github.com/jessesteele/yeo
 cd yeo
